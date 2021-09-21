587af06e5161f269f2251cccd70c1643b4acf33a
Tristan Dunn <tristanzdunn@gmail.com>
2021-09-21 18:56:41 -0400

    Simplify feature tests and use more descriptive naming.

--- spec/features/commands/unknown_spec.rb
+++ spec/features/commands/unknown_spec.rb
@@ -3,29 +3,28 @@
 require "rails_helper"
 
 describe "Sending an unknown command", type: :feature, js: true do
-  let(:bob)  { create(:character, room: room) }
-  let(:room) { create(:room) }
-  let(:sue)  { create(:character, room: room) }
+  let(:character) { create(:character) }
+  let(:command)   { "/fake command" }
 
   before do
-    sign_in_as_character bob
+    sign_in_as_character character
   end
 
   it "displays the unknown command message to the sender" do
-    send_text("/fake command")
+    send_text(command)
 
-    expect(page).to have_unknown_command_message("/fake command")
+    expect(page).to have_unknown_command_message(command)
   end
 
   it "does not broadcast the message to the room" do
-    using_session(sue) do
-      sign_in_as_character sue
+    using_session(:nearby_character) do
+      sign_in_as_character create(:character, room: character.room)
     end
 
-    send_text("/fake command")
+    send_text(command)
 
-    using_session(sue) do
-      expect(page).not_to have_unknown_command_message("/fake command")
+    using_session(:nearby_character) do
+      expect(page).not_to have_unknown_command_message(command)
     end
   end
 
