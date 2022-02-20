96904b933f91d3996bf531f66f4403f34968b172
Tristan Dunn <tristanzdunn@gmail.com>
2022-02-20 10:32:15 -0500

    Update the database schema to Rails 7.

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_203840) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_01_16_203840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", limit: 255, null: false
     t.string "password_digest", limit: 60, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
   end
 
   create_table "characters", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.string "name", limit: 12, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "room_id", null: false
     t.integer "level", default: 1, null: false
     t.integer "experience", default: 0, null: false
-    t.datetime "active_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "active_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.boolean "playing", default: false, null: false
     t.index ["account_id"], name: "index_characters_on_account_id"
     t.index ["active_at", "playing"], name: "index_characters_on_active_at_and_playing"
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 2022_01_16_203840) do
     t.bigint "y", null: false
     t.bigint "z", null: false
     t.string "description", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["x", "y", "z"], name: "index_rooms_on_x_and_y_and_z", unique: true
   end
 
