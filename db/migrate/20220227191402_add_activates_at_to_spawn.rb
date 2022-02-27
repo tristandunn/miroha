590409872cce9926d70e1ff2708e26ca575f01e9
Tristan Dunn <tristanzdunn@gmail.com>
2022-02-27 15:45:54 -0500

    Add clock service for activating spawns.

---
+++ db/migrate/20220227191402_add_activates_at_to_spawn.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddActivatesAtToSpawn < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def change
+    add_column :spawns, :activates_at, :datetime
+    add_index :spawns, :activates_at, where: "entity_id IS NULL", algorithm: :concurrently
+  end
+end
