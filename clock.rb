9091e9a6440e58bf17e7bd01a2b8d093a74dccff
Tristan Dunn <tristanzdunn@gmail.com>
2022-03-28 22:30:54 -0400

    Switch from zhong to rufus-scheduler for clock management.

--- clock.rb
+++ clock.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
-require "zhong"
-require Rails.root.join("config/clock")
+Rufus::Scheduler.singleton.tap do |schedule|
+  %w(INT TERM).each do |signal|
+    handler = Signal.trap(signal) do
+      schedule.shutdown(
+        wait: ENV.fetch("CLOCK_SHUTDOWN_WAIT_SECONDS", 29).to_i
+      )
 
-Zhong.start
+      if handler.respond_to?(:call)
+        handler.call
+      else
+        exit
+      end
+    end
+  end
+
+  schedule.join
+end
