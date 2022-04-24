b6094a1bd623e6fa48c38f9d76500e8812ab8730
Tristan Dunn <tristanzdunn@gmail.com>
2022-04-24 15:15:17 -0400

    Update dependencies.
    * Updates Ruby version to 3.1.2.
    * Updates Node version to 17.9.0.
    * Updates bundler, rack-attack, rspec-rails, rubocop, rubocop-rspec in Ruby.
    * Updates esbuild, eslint, rustywind, sinon, and stylelint in JavaScript.
    * Removes turbo_stream_from patch for ID attribute.

--- app/controllers/concerns/basic_authentication.rb
+++ app/controllers/concerns/basic_authentication.rb
@@ -12,8 +12,8 @@ module BasicAuthentication
   # @return [void]
   def basic_authentication
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTHENTICATION_USERNAME"] &&
-        password == ENV["BASIC_AUTHENTICATION_PASSWORD"]
+      username == ENV.fetch("BASIC_AUTHENTICATION_USERNAME", nil) &&
+        password == ENV.fetch("BASIC_AUTHENTICATION_PASSWORD", nil)
     end
   end
 
