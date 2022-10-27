99bb2ab2a61bfe7a3c37aed3b11b62ed7be7a76b
Tristan Dunn <tristanzdunn@gmail.com>
2022-10-26 20:39:50 -0400

    Use a connection pool for Redis cache store and objects.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store
+  config.cache_store = :redis_cache_store, { pool_size: 5, pool_timeout: 5 }
 
   # Use a real queuing backend for Active Job and separate queues
   # per environment.
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Redis::Objects.redis = Redis.new
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new }
