99419b3c102d9fd9f12ef0830ad0d0125b548a14
Tristan Dunn <tristanzdunn@gmail.com>
2024-02-17 11:48:05 -0500

    Display notice when stream is disconnected.

--- spec/views/game/_streams.html.erb_spec.rb
+++ spec/views/game/_streams.html.erb_spec.rb
@@ -12,21 +12,31 @@ describe "game/_streams.html.erb" do
 
   let(:character) { build_stubbed(:character) }
 
-  it "renders the character stream" do
-    expect(html).to have_stream(id: dom_id(character))
+  it "attaches the streams controller" do
+    expect(html).to have_css(%([data-controller=streams] #streams))
+  end
+
+  it "renders the character stream with a target" do
+    expect(html).to have_stream(id: dom_id(character), attributes: "[data-streams-target=stream]")
   end
 
   it "renders the chat stream" do
     expect(html).to have_stream(id: dom_id(character.room))
   end
 
+  it "renders the streams controller notice" do
+    expect(html).to have_css("[data-streams-target=notice] span", text: t("game.streams.title")).and(
+      have_css("[data-streams-target=notice] span", text: t("game.streams.message"))
+    )
+  end
+
   protected
 
-  def have_stream(id:)
+  def have_stream(id:, attributes: nil)
     have_css(
       <<~SELECTOR.strip
         #streams
-        turbo-cable-stream-source[channel="Turbo::StreamsChannel"][id="#{id}"]
+        turbo-cable-stream-source[channel="Turbo::StreamsChannel"][id="#{id}"]#{attributes}
       SELECTOR
     )
   end
