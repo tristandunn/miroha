9e4be613439df8ef3eff811f92bf9a572f7bb03d
Tristan Dunn <tristanzdunn@gmail.com>
2024-02-17 11:48:05 -0500

    WIP: Track the stream connection status.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus";
 import ChatController from "./chat_controller";
 import GameController from "./game_controller";
+import StreamsController from "./streams_controller";
 
 const application = Application.start();
 
 application.register("chat", ChatController);
 application.register("game", GameController);
+application.register("streams", StreamsController);
