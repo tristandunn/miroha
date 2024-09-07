ada7e36d5f5bd83524fa8ef3e61b6e467ffa9135
Tristan Dunn <tristanzdunn@gmail.com>
2024-09-07 17:15:54 -0400

    Add support for running tests via Docker Compose.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,5 +2,15 @@
 
 require "capybara"
 
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--disable-site-isolation-trials")
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
 Capybara.server = :puma, { Silent: true }
 Capybara.javascript_driver = :selenium_chrome_headless
