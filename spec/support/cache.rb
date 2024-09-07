a92f45eac5ef5610b3b2970a64735de07d96edb8
Tristan Dunn <tristanzdunn@gmail.com>
2024-09-07 18:11:00 -0400

    Switch back to a null cache store in tests by default.

--- spec/support/cache.rb
+++ spec/support/cache.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before do
-    Rails.cache.clear
+  config.before(cache: true) do
+    allow(Rails).to receive(:cache).and_return(ActiveSupport::Cache::MemoryStore.new)
   end
 end
