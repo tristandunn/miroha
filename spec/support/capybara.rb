ee85901dedde15735946d2eb26ec300c6f5e4258
Tristan Dunn <tristanzdunn@gmail.com>
2024-09-12 22:37:39 -0400

    Adjust how the customer driver is registered.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,14 +2,19 @@
 
 require "capybara"
 
-Capybara.register_driver :selenium_chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--disable-site-isolation-trials")
-  options.add_argument("--headless=new")
-  options.add_argument("--no-sandbox")
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+Capybara.register_driver :selenium_chrome_headless do |application|
+  Capybara::Selenium::Driver.new(
+    application,
+    browser: :chrome,
+    options: Selenium::WebDriver::Chrome::Options.new(
+      args: [
+        "--disable-dev-shm-usage",
+        "--disable-site-isolation-trials",
+        "--headless=new",
+        "--no-sandbox"
+      ]
+    )
+  )
 end
 
 Capybara.server = :puma, { Silent: true }
