b0e6511b431bd98164b5434eb22caf24bf1413ce
Tristan Dunn <tristanzdunn@gmail.com>
2024-12-14 18:07:33 -0500

    Update dependencies.
    * Updates Node version to 22.12.0.
    * Updates Yarn version to 4.5.3.
    * Updates dotenv, oj, rails, rubocop, and rubocop-rspec in Ruby.
    * Updates @stylistic/eslint-plugin-js, c8, and eslint in JavaScript.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,9 +3,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
   # Do not eager load code on boot.
@@ -36,12 +34,6 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
-
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
