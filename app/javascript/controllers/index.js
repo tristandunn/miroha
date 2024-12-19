e7e49ae819b7330bf62ac24b97b4c8de5e99fa4f
Tristan Dunn <tristanzdunn@gmail.com>
2024-12-18 22:31:16 -0500

    WIP

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,4 @@
-import { Application } from "@hotwired/stimulus";
-import ChatController from "./chat_controller";
-import GameController from "./game_controller";
-import StreamsController from "./streams_controller";
+import { application } from "controllers/application";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
-const application = Application.start();
-
-application.register("chat", ChatController);
-application.register("game", GameController);
-application.register("streams", StreamsController);
+eagerLoadControllersFrom("controllers", application);
