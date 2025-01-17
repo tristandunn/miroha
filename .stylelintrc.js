51592bb3ab3a22da41ca154ff933ad0df5d63ed2
Tristan Dunn <tristanzdunn@gmail.com>
2025-01-17 13:01:24 -0500

    Update dependencies.
    * Updates Yarn version to 4.6.0.
    * Updates brakeman, bundler, erb_lint, rubocop, rubocop-performance,
      tailwindcss-rails, thruster in Ruby.
    * Updates @stylistic/eslint-plugin-js, eslint, jsdom, mocha, stylelint, and
      stylelint-config-standard in JavaScript.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,12 @@ export default {
   "extends": ["stylelint-config-standard"],
 
   "rules": {
+    "at-rule-no-deprecated": [
+      true,
+      {
+        "ignoreAtRules": ["apply"]
+      }
+    ],
     "at-rule-no-unknown": [
       true,
       {
