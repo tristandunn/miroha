4969de76ce35ad49674fe7287176675c03fd9d2c
Tristan Dunn <hello@tristandunn.com>
2025-11-12 22:35:17 -0500

    Update dependencies.
    * Updates Node to 24.11.1.
    * Updates Yarn to 4.11.0.
    * Updates brakeman, kamal, rails, rubocop, rubocop-factory_bot, rubocop-rspec,
      rubocop-rspec_rails, selenium-webdriver, shoulda-matchers, solid_cache,
      solid_queue, sqlite3, tailwindcss-rails, thruster, and turbo-rails in Ruby.
    * Updates chai, eslint, jsdom, and mocha in JavaScript.
    * Updates actions/setup-node in GitHub Actions.
    * Add bundler-audit in Ruby.

--- Gemfile
+++ Gemfile
@@ -5,21 +5,22 @@ source "https://rubygems.org"
 
 gem "bcrypt",            "3.1.20"
 gem "bootsnap",          "1.18.6", require: false
+gem "bundler-audit",     "0.9.2",  require: false
 gem "hotwire-rails",     "0.1.3"
 gem "importmap-rails",   "2.2.2"
-gem "kamal",             "2.7.0"
+gem "kamal",             "2.8.2"
 gem "propshaft",         "1.3.1"
 gem "puma",              "7.1.0"
 gem "rack-timeout",      "0.7.0"
-gem "rails",             "8.0.3"
+gem "rails",             "8.1.1"
 gem "solid_cable",       "3.0.12"
-gem "solid_cache",       "1.0.8"
-gem "solid_queue",       "1.2.1"
-gem "sqlite3",           "2.7.4"
+gem "solid_cache",       "1.0.10"
+gem "solid_queue",       "1.2.4"
+gem "sqlite3",           "2.8.0"
 gem "stimulus-rails",    "1.3.4"
-gem "tailwindcss-rails", "4.3.0"
-gem "thruster",          "0.1.15", require: false
-gem "turbo-rails",       "2.0.17"
+gem "tailwindcss-rails", "4.4.0"
+gem "thruster",          "0.1.16", require: false
+gem "turbo-rails",       "2.0.20"
 
 group :development, :test do
   gem "cacheflow",   "0.5.0"
@@ -27,18 +28,18 @@ group :development, :test do
 end
 
 group :development do
-  gem "brakeman",            "7.1.0", require: false
+  gem "brakeman",            "7.1.1", require: false
   gem "erb_lint",            "0.9.0", require: false
   gem "listen",              "3.9.0"
   gem "rack-mini-profiler",  "4.0.1"
-  gem "rubocop",             "1.81.1", require: false
+  gem "rubocop",             "1.81.7", require: false
   gem "rubocop-capybara",    "2.22.1", require: false
-  gem "rubocop-factory_bot", "2.27.1", require: false
+  gem "rubocop-factory_bot", "2.28.0", require: false
   gem "rubocop-performance", "1.26.1", require: false
   gem "rubocop-rails",       "2.33.4", require: false
   gem "rubocop-rake",        "0.7.1",  require: false
-  gem "rubocop-rspec",       "3.7.0",  require: false
-  gem "rubocop-rspec_rails", "2.31.0", require: false
+  gem "rubocop-rspec",       "3.8.0",  require: false
+  gem "rubocop-rspec_rails", "2.32.0", require: false
   gem "web-console",         "4.2.1"
 end
 
@@ -49,7 +50,7 @@ group :test do
   gem "factory_bot_rails",        "6.5.1"
   gem "faker",                    "3.5.2"
   gem "rails-controller-testing", "1.0.5"
-  gem "selenium-webdriver",       "4.37.0"
-  gem "shoulda-matchers",         "6.5.0"
+  gem "selenium-webdriver",       "4.38.0"
+  gem "shoulda-matchers",         "7.0.1"
   gem "simplecov-console",        "0.9.4", require: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_29_234433) do
+ActiveRecord::Schema[8.1].define(version: 2025_07_29_234433) do
   create_table "accounts", force: :cascade do |t|
+    t.json "aliases", default: {}, null: false
+    t.datetime "created_at", null: false
     t.string "email", limit: 255, null: false
     t.string "password_digest", limit: 60, null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "aliases", default: {}, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
   end
 
   create_table "characters", force: :cascade do |t|
     t.integer "account_id", null: false
-    t.string "name", limit: 12, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "room_id", null: false
-    t.integer "level", default: 1, null: false
-    t.integer "experience", default: 0, null: false
     t.datetime "active_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.boolean "playing", default: false, null: false
+    t.datetime "created_at", null: false
     t.integer "current_health", default: 10, null: false
+    t.integer "experience", default: 0, null: false
+    t.integer "level", default: 1, null: false
     t.integer "maximum_health", default: 10, null: false
+    t.string "name", limit: 12, null: false
+    t.boolean "playing", default: false, null: false
+    t.integer "room_id", null: false
+    t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_characters_on_account_id"
     t.index ["active_at", "playing"], name: "index_characters_on_active_at_and_playing"
     t.index ["name"], name: "index_characters_on_name", unique: true
@@ -40,59 +40,59 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_29_234433) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string "owner_type", null: false
-    t.integer "owner_id", null: false
-    t.string "name", limit: 24, null: false
     t.datetime "created_at", null: false
+    t.string "name", limit: 24, null: false
+    t.integer "owner_id", null: false
+    t.string "owner_type", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_type", "owner_id"], name: "index_items_on_owner"
   end
 
   create_table "monsters", force: :cascade do |t|
-    t.string "name", limit: 24, null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "current_health", default: 5, null: false
-    t.integer "maximum_health", default: 5, null: false
-    t.integer "experience", default: 0, null: false
-    t.integer "room_id"
     t.json "event_handlers", default: [], null: false
+    t.integer "experience", default: 0, null: false
+    t.integer "maximum_health", default: 5, null: false
+    t.string "name", limit: 24, null: false
+    t.integer "room_id"
+    t.datetime "updated_at", null: false
     t.index ["event_handlers"], name: "index_monsters_on_event_handlers"
     t.index ["room_id"], name: "index_monsters_on_room_id"
     t.check_constraint "current_health <= maximum_health", name: "monsters_current_health_check"
   end
 
   create_table "npcs", force: :cascade do |t|
-    t.integer "room_id"
-    t.string "name", limit: 24, null: false
     t.datetime "created_at", null: false
+    t.string "name", limit: 24, null: false
+    t.integer "room_id"
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_npcs_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "description", null: false
+    t.json "objects", default: {}, null: false
+    t.datetime "updated_at", null: false
     t.bigint "x", null: false
     t.bigint "y", null: false
     t.bigint "z", null: false
-    t.string "description", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.json "objects", default: {}, null: false
     t.index ["x", "y", "z"], name: "index_rooms_on_x_and_y_and_z", unique: true
   end
 
   create_table "spawns", force: :cascade do |t|
-    t.string "base_type", null: false
-    t.integer "base_id", null: false
-    t.string "entity_type"
-    t.integer "entity_id"
-    t.integer "room_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.datetime "activates_at"
-    t.datetime "expires_at"
+    t.integer "base_id", null: false
+    t.string "base_type", null: false
+    t.datetime "created_at", null: false
     t.bigint "duration"
+    t.integer "entity_id"
+    t.string "entity_type"
+    t.datetime "expires_at"
     t.bigint "frequency"
+    t.integer "room_id", null: false
+    t.datetime "updated_at", null: false
     t.index ["activates_at"], name: "index_spawns_on_activates_at", where: "entity_id IS NULL"
     t.index ["base_type", "base_id"], name: "index_spawns_on_base"
     t.index ["entity_type", "entity_id"], name: "index_spawns_on_entity"
